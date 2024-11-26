944b701ccd5f2d769f85aad0119e4f5e8b36ee75
sulaiman <mohammedalrabeai@gmail.com>
2024-11-26 16:49:57 +0300

    fix shift times

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -30,4 +30,62 @@ class Shift extends Model
     {
         return $this->belongsTo(Zone::class);
     }
+
+        // تخزين القيم بالدقائق كوقت
+        public function setEarlyEntryTimeAttribute($value)
+        {
+            $this->attributes['early_entry_time'] = gmdate('H:i:s', $value * 60);
+        }
+    
+        public function setLastEntryTimeAttribute($value)
+        {
+            $this->attributes['last_entry_time'] = gmdate('H:i:s', $value * 60);
+        }
+    
+        public function setEarlyExitTimeAttribute($value)
+        {
+            $this->attributes['early_exit_time'] = gmdate('H:i:s', $value * 60);
+        }
+    
+        public function setLastTimeOutAttribute($value)
+        {
+            $this->attributes['last_time_out'] = gmdate('H:i:s', $value * 60);
+        }
+    
+        public function getEarlyEntryTimeAttribute($value)
+        {
+            if ($value) {
+                $parts = explode(':', $value); // فصل الساعات والدقائق والثواني
+                return ($parts[0] * 60) + $parts[1]; // حساب الدقائق (الساعات × 60) + الدقائق
+            }
+            return null;
+        }
+        
+        public function getLastEntryTimeAttribute($value)
+        {
+            if ($value) {
+                $parts = explode(':', $value);
+                return ($parts[0] * 60) + $parts[1];
+            }
+            return null;
+        }
+        
+        public function getEarlyExitTimeAttribute($value)
+        {
+            if ($value) {
+                $parts = explode(':', $value);
+                return ($parts[0] * 60) + $parts[1];
+            }
+            return null;
+        }
+        
+        public function getLastTimeOutAttribute($value)
+        {
+            if ($value) {
+                $parts = explode(':', $value);
+                return ($parts[0] * 60) + $parts[1];
+            }
+            return null;
+        }
+        
 }
